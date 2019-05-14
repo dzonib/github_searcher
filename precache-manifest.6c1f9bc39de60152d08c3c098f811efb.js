5f97651f8f60d241a5a9ba9bd08c49bf72912ba8
dzonib <nikolabosnjak381@gmail.com>
2019-05-14 23:13:38 +0200

    Updates

---
+++ precache-manifest.6c1f9bc39de60152d08c3c098f811efb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edf6edb96eb28f6620bc00dd82741a4",
+    "url": "/github_searcher/index.html"
+  },
+  {
+    "revision": "c903509249d29cb546db",
+    "url": "/github_searcher/static/js/2.24d8ea41.chunk.js"
+  },
+  {
+    "revision": "1f3be4df820d4061932d",
+    "url": "/github_searcher/static/js/main.08fdb54c.chunk.js"
+  },
+  {
+    "revision": "5f26258f147a1f6c87c1",
+    "url": "/github_searcher/static/js/runtime~main.9c6e494f.js"
+  },
+  {
+    "revision": "9b54dbe80bed2ddbb8788c3f421b0973",
+    "url": "/github_searcher/static/media/fidget-spinner-loading.9b54dbe8.gif"
+  }
+]);
